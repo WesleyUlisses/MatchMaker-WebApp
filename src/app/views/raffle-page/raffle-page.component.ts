67a737080c27d22798d9e69d54b43f1c3abcdc6d
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-06-08 14:11:03 -0300

    style: Update raffle page styles and fonts

--- src/app/views/raffle-page/raffle-page.component.ts
+++ src/app/views/raffle-page/raffle-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderHomeComponent } from '../../shared/components/header-home/header-home.component';
 import { CommonModule } from '@angular/common';
+import { RaffleContainerComponent } from '../../shared/components/raffle-container/raffle-container.component';
 
 @Component({
   selector: 'app-raffle-page',
   standalone: true,
-  imports: [HeaderHomeComponent, CommonModule],
+  imports: [HeaderHomeComponent, CommonModule, RaffleContainerComponent],
   templateUrl: './raffle-page.component.html',
   styleUrl: './raffle-page.component.css'
 })
 export class RafflePageComponent {
-
+  
 }
